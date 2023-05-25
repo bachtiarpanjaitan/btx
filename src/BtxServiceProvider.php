d30f5378af2d9e248daec8b16214a7da46fc1e22
bachtiarpanjaitan <bachtiarpanjaitan0@gmail.com>
2023-05-25 16:25:20 +0700

    Restructure

---
+++ src/BtxServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Btx;
+
+use Illuminate\Support\ServiceProvider;
+
+class BtxFilterServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // include __DIR__.'/Constants/Operator.php';
+        $this->publishes([
+            __DIR__.'/config/btx.php' => config_path('btx.php'),
+        ], 'config');
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom( __DIR__.'/config/btx.php', 'btx');
+    }
+}
