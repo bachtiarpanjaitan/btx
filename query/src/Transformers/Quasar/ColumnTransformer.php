99c58e8b38a1d8baa71bc05fcb89cab742fc3581
bachtiarpanjaitan <bachtiarpanjaitan0@gmail.com>
2023-06-01 09:11:36 +0700

    Update ColumTransformer for Quasar

--- query/src/Transformers/Quasar/ColumnTransformer.php
+++ query/src/Transformers/Quasar/ColumnTransformer.php
@@ -3,19 +3,25 @@
 namespace Btx\Query\Transformers\Quasar;
 
 use League\Fractal\TransformerAbstract;
-
 class ColumnTransformer extends TransformerAbstract {
 
     public function transform($resp) {
         $align = 'left';
+        $type = 'text';
+        $show = true;
         if(isset($resp['align'])) $align = $resp['align'];
+        if(isset($resp['type'])) $type = $resp['type'];
+        if(isset($resp['show'])) $show = $resp['show'];
         return [
             'name' => $resp['value'],
             'required' => true,
             'label' => ucwords(implode(' ',explode('_',ucfirst($resp['label'])))),
             'align' =>  $align,
             'field' => $resp['value'],
-            'sortable' => true
+            'sortable' => true,
+            'type' => $type,
+            'options' => isset($resp['options']) ? $resp['options'] : [],
+            'show' => $show
         ];
     }
 }
