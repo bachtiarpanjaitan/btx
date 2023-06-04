c923ef9dcc983b53697ca0e8d06e4bc836d4e2a4
Bachtiar Panjaitan <bachtiarpanjaitan0@gmail.com>
2023-06-04 11:31:37 +0700

    Fixing Map Class

--- common/src/Map.php
+++ common/src/Map.php
@@ -2,6 +2,8 @@
 
 namespace Btx\Common;
 
+use Exception;
+
 /**
  * Map Calculation
  * @author bachtiarpanjaitan <bachtiarpanjaitan0@gmail.com>
@@ -12,19 +14,22 @@ class Map {
     /**
      * To determine the coordinates are within a predetermined radius in meter
      * @param Array $needle coordinate [lat, lng]
-     * @param Object $haystack coordinates 
+     * @param Array $haystack coordinates 
      * [radius: 0, lat: 0.0, lng: 0.0]
      * 
      */
-    public static function onCoordinateRadius(array $needle, object $haystack) : bool{
+    public static function onCoordinateRadius(array $needle, array $haystack) : bool{
         $result = false;
-
-        $radius = (int) $haystack['radius'];
-        $centerX = (double)$haystack['lat'];
-        $centerY = (double) $haystack['lng'];
-        $distance = (float) self::distanceInMeter((double) $needle[0],(double) $needle[1],$centerX,$centerY);
-        if ($distance <= $radius) $result = true;
-        return $result;
+        try {
+            $radius = (int) $haystack['radius'];
+            $centerX = (double)$haystack['lat'];
+            $centerY = (double) $haystack['lng'];
+            $distance = (float) self::distanceInMeter((double) $needle[0],(double) $needle[1],$centerX,$centerY);
+            if ($distance <= $radius) $result = true;
+            return $result;
+        }catch(Exception $e){
+            throw $e;
+        }
     }
 
     /**
@@ -37,8 +42,8 @@ class Map {
         $dLon = deg2rad($lon2 - $lon1);
         $a = sin($dLat/2) * sin($dLat/2) + cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * sin($dLon/2) * sin($dLon/2);
         $c = 2 * atan2(sqrt($a), sqrt(1 - $a));
-        $distance = $this->_earthRadius * $c;
-        return $distance;
+        $distance = self::$_earthRadius * $c;
+        return $distance;   
     }
 
 }
