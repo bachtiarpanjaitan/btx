23527f908067717a7ad9f1411ccda8310ba7f884
Bachtiar Panjaitan <bachtiarpanjaitan0@gmail.com>
2023-12-16 12:48:03 +0700

    Making Trait Static

--- http/src/Traits/StaticResponse.php
+++ http/src/Traits/StaticResponse.php
@@ -14,7 +14,7 @@ trait StaticResponse {
      * memberikan sebuah respon palsu guna hanya untuk testing response
      * @return array
      */
-    public function fakeResponse($status = true, $message = 'Fake Response', $data = null) {
+    public static function fakeResponse($status = true, $message = 'Fake Response', $data = null) {
         return ApiResponse::make($status, $message, $data);
     }
     
@@ -22,7 +22,7 @@ trait StaticResponse {
      * memberikan status default 301 (Moved Permanently)
      * @return array
      */
-    public function response301(string $appendText = '', string $appendTextDir = '') {
+    public static function response301(string $appendText = '', string $appendTextDir = '') {
         ApiResponse::setStatusCode(301);
         ApiResponse::setAppendMessage($appendText);
         if (!empty($appendTextDir)) {
@@ -31,7 +31,7 @@ trait StaticResponse {
         return ApiResponse::make(false, '', null);
     }
 
-    public function response200(string $appendText = '',$data = null, $appendData = null){
+    public static function response200(string $appendText = '',$data = null, $appendData = null){
         if(!empty($appendData)) ApiResponse::setIncludeData($appendData);
         return ApiResponse::make(true, $appendText, $data);
     }
@@ -40,7 +40,7 @@ trait StaticResponse {
      * memberikan status default 400 (Bad request)
      * @return array
      */
-    public function response400(string $appendText = '', string $appendTextDir = '') {
+    public static function response400(string $appendText = '', string $appendTextDir = '') {
         ApiResponse::setStatusCode(400, false);
         ApiResponse::setAppendMessage($appendText);
         if (!empty($appendTextDir)) {
@@ -53,7 +53,7 @@ trait StaticResponse {
      * memberikan status default 401 (error Unauthorized)
      * @return array
      */
-    public function response401() {
+    public static function response401() {
         ApiResponse::setStatusCode(401);
         return ApiResponse::make(false, '', null);
     }
@@ -62,7 +62,7 @@ trait StaticResponse {
      * memberikan status default 404 (error not found)
      * @return array
      */
-    public function response404(string $appendText = '', string $appendTextDir = '') {
+    public static function response404(string $appendText = '', string $appendTextDir = '') {
         ApiResponse::setStatusCode(404);
         ApiResponse::setAppendMessage($appendText);
         if (!empty($appendTextDir)) {
@@ -75,7 +75,7 @@ trait StaticResponse {
      * memberikan status default 405 (method not allowed)
      * @return array
      */
-    public function response405(string $appendText = '', string $appendTextDir = '') {
+    public static function response405(string $appendText = '', string $appendTextDir = '') {
         ApiResponse::setStatusCode(405);
         ApiResponse::setAppendMessage($appendText);
         if (!empty($appendTextDir)) {
@@ -88,7 +88,7 @@ trait StaticResponse {
      * memberikan status default 500 (error disisi server)
      * @return array
      */
-    public function response500(string $appendText = '', $data = []) {
+    public static function response500(string $appendText = '', $data = []) {
         ApiResponse::setStatusCode(500);
         ApiResponse::setAppendMessage($appendText);
         ApiResponse::setIncludeData($data);
